647c3733d4334a66876dbd9c5c42a29815d9b246
Bauluccy <fifoluciano@hotmail.com.br>
2022-05-25 17:26:24 -0300

    alpha 0.1

---
+++ app/src/main/java/com/example/finalproject/TelaSupervisor.java
@@ -0,0 +1,14 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaSupervisor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_supervisor);
+    }
+}
