3b724b3805ce6d87c1815387fa2a2c6a7f15636c
Bauluccy <fifoluciano@hotmail.com.br>
2022-05-26 11:56:55 -0300

    alpha 0.1

---
+++ app/src/main/java/com/example/finalproject/TelaCadastro.java
@@ -0,0 +1,18 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+
+    public void cadastrarUsuario(View view) {
+    }
+}
